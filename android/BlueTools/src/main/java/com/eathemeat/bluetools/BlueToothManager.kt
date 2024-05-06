fd50805ebb6b9eb9df26119de0b385ecfdf7f429
xipeitao <xipeitao@yy.com>
2024-05-07 07:55:32 +0800

    new

--- android/BlueTools/src/main/java/com/eathemeat/bluetools/BlueToothManager.kt
+++ android/BlueTools/src/main/java/com/eathemeat/bluetools/BlueToothManager.kt
@@ -1,9 +1,15 @@
 package com.eathemeat.bluetools
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 
 /**
  * author:PeterX
@@ -15,6 +21,27 @@ class BlueToothManager(val context:Context) {
 
     val blueToothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
     var  blueToothAdapter: BluetoothAdapter?
+
+    val receiver = object :BroadcastReceiver() {
+
+        @SuppressLint("MissingPermission")
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        override fun onReceive(p0: Context, p1: Intent) {
+            val action:String? = p1.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND ->{
+                    val device: BluetoothDevice? = p1.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE,BluetoothDevice::class.java)
+                    device?.let {
+                        val devicesName = device.name
+                        val deviceHAddress = device.address
+                        val deviceAlias = device.alias
+                    }
+
+
+                }
+            }
+        }
+    }
 
     init {
         blueToothAdapter = blueToothManager.adapter
