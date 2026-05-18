377e0cbce10f20f535dd8d72b8f97e83e8e9a852
PeterXi <peter12757@126.com>
2026-05-18 10:23:34 +0800

    new

--- android/app/src/main/java/com/eathemeat/transdroid/main/ui/screen/home/HomeSate.kt
+++ android/app/src/main/java/com/eathemeat/transdroid/main/ui/screen/home/HomeSate.kt
@@ -1,4 +1,19 @@
 package com.eathemeat.transdroid.main.ui.screen.home
 
+import androidx.compose.ui.graphics.ImageBitmap
+import java.util.UUID
+
+data class NavigitionItem(val title:String, val icon: ImageBitmap?= null)
+
 class HomeSate {
+    val navItems: MutableList<NavigitionItem> = mutableListOf<NavigitionItem>(
+        NavigitionItem("蓝牙"),
+        NavigitionItem("Wifi"),
+        NavigitionItem("红外"),
+        NavigitionItem("其他")
+    )
+
+    val curNavItem = navItems[1]
+
+
 }
--- android/app/src/main/java/com/eathemeat/transdroid/main/ui/screen/home/HomeScreen.kt
+++ android/app/src/main/java/com/eathemeat/transdroid/main/ui/screen/home/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.eathemeat.transdroid.main.ui.screen.home
 
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.eathemeat.transdroid.main.MainModel
 import com.eathemeat.transdroid.main.ui.theme.TransDroidTheme
 
@@ -15,8 +20,24 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     mainModel: MainModel = viewModel(),
 ) {
-    ConstraintLayout() {
+    val navController = rememberNavController()
 
+    ConstraintLayout() {
+        val (nav,host) = createRefs()
+        LazyRow(modifier = modifier.constrainAs(nav){
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
+            top.linkTo(host.bottom,5.dp)
+        }) {
+            items(mainModel.homeState.navItems){ item ->
+                // TODO:  
+                Card() { }
+            }
+        }
+        NavHost(navController, startDestination = mainModel.homeState.curNavItem.title) {
+            // TODO:
+        }
     }
 }
 
